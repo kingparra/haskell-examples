4ea28eacf53c90f8768e4fe874c1af87ff658ada
Chris King-Parra <chris@kingparra.work>
2023-09-03 16:21:29 -0400

    Add example of feting a url

---
+++ DownloadUrl.hs
@@ -0,0 +1,15 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-20.18 script --package http-conduit --package bytestring
+{-# LANGUAGE OverloadedStrings #-}
+
+import Network.HTTP.Simple
+import qualified Data.ByteString.Char8 as B8
+
+main :: IO ()
+main = do
+  let url = "https://en.wikipedia.org/wiki/Sinking_of_SS_Princess_Alice"
+  let urlParts = B8.split '/' $ B8.pack url
+  let fileName = Prelude.last urlParts
+  req <- parseRequest url
+  resp <- httpBS req
+  B8.putStrLn $ getResponseBody resp
